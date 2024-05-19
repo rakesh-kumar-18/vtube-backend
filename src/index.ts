5b3230be9976c741aa0f3b59e6fc27571194d938
Rakesh Kumar Sahoo <sahoorakeshkumar42@gmail.com>
2024-05-19 09:57:38 +0530

    connected to mongodb database

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
-import express, { Express, Request, Response } from "express";
 import "dotenv/config";
+import express, { Express, Request, Response } from "express";
+import connectDB from "./db";
 
 const app: Express = express();
 const port = process.env.PORT || 8000;
+
+connectDB();
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello World!");
